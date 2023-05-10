bd26b16a11f264456891f22cde8b266f34648cc5
Roger Paris <roger.parisvicent@gmail.com>
2023-05-10 16:37:22 +0200

    get user from repo

--- lib/src/features/authentication/data/fake_auth_repository.dart
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -22,8 +22,6 @@ class FakeAuthRepository {
   }
 
   Future<void> signOut() async {
-    // await Future.delayed(const Duration(seconds: 3));
-    // throw Exception('Connection failed');
     _authState.value = null;
   }
 
