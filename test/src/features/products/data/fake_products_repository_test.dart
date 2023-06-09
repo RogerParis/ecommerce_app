bed93fa09a49c34a6c20b252e8439affbb39d301
Roger Paris <roger.parisvicent@gmail.com>
2023-06-09 13:24:51 +0200

    adds first test

---
+++ test/src/features/products/data/fake_products_repository_test.dart
@@ -0,0 +1,10 @@
+import 'package:ecommerce_app/src/constants/test_products.dart';
+import 'package:ecommerce_app/src/features/products/data/fake_products_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('getProductsList returns global list', () {
+    final productsRepository = FakeProductsRepository();
+    expect(productsRepository.getProductsList(), kTestProducts);
+  });
+}
