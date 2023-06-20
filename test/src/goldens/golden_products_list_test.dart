5e3baad6bf585a5a483981270c2f8ea9c28d2480
Roger Paris <roger.parisvicent@gmail.com>
2023-06-20 18:20:01 +0200

    uses variants and tags for tests

---
+++ test/src/goldens/golden_products_list_test.dart
@@ -0,0 +1,33 @@
+import 'package:ecommerce_app/src/app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../robot.dart';
+
+void main() {
+  final sizeVariant = ValueVariant<Size>({
+    const Size(300, 600),
+    const Size(600, 800),
+    const Size(1000, 1000),
+  });
+  testWidgets(
+    'Golden - products list',
+    (tester) async {
+      final r = Robot(tester);
+      final currentSize = sizeVariant.currentValue!;
+      await r.golden.setSurfaceSize(currentSize);
+      await r.golden.loadRobotoFont();
+      await r.golden.loadMaterialIconFont();
+      await r.pumpMyApp();
+      await r.golden.precacheImages();
+      await expectLater(
+        find.byType(MyApp),
+        matchesGoldenFile(
+          'products_list_${currentSize.width.toInt()}x${currentSize.height.toInt()}.png',
+        ),
+      );
+    },
+    variant: sizeVariant,
+    tags: ['golden'],
+  );
+}
